34a01c3b7a5996eaaba64cb87b4aa543dd03a23c
Vitorvgc <vitorvgc07@gmail.com>
2016-07-21 21:08:17 -0300

    first commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,37 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
+
+public class Controller {
+
+    @FXML
+    AnchorPane cenario;
+
+    @FXML
+    TextArea log;
+
+    @FXML
+    TableView tabelaClientes;
+
+    public Controller() {
+
+    }
+
+    @FXML
+    private void novoSistema() {
+
+    }
+
+    @FXML
+    private void novoCliente() {
+
+    }
+
+    @FXML
+    private void reset() {
+
+    }
+}
---
+++ src/sample/PersonagemView.java
@@ -0,0 +1,16 @@
+package sample;
+
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+
+/**
+ * Created by Vítor on 21/07/2016.
+ */
+public class PersonagemView extends VBox {
+
+    Label senha;
+    Label id;
+    ImageView sprite;
+
+}
